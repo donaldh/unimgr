e7dd8661bb06c08ae47aefec1dc57c2f3f19db01
santanu.de <santanu.de@xoriant.com>
2018-03-22 13:35:01 +0100

    commit Legato changes

---
+++ legato-api/src/main/java/org/opendaylight/legato/util/LegatoConstants.java
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c) 2014 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.legato.util;
+
+public class LegatoConstants {
+
+	public static final String EVC_ID = "EVC_ID";
+	public static final String EVC_MAX_FRAME = "EVC_MAX_FRAME";
+	public static final String EVC_STATUS = "EVC_STATUS";
+	public static final String EVC_UNI_LIST = "EVC_UNI_LIST";
+	public static final String EVC_CON_TYPE = "EVC_CON_TYPE";
+	public static final String BEST_EFFORT = "BEST_EFFORT";
+	public static final long LONG_VAL = 9223372;
+	public static final String SLS_PROFILES = "SLS_PROFILES";
+	public static final String COS_PROFILES = "COS_PROFILES";
+	public static final String BWP_PROFILES = "BWP_PROFILES";
+	public static final String POINTTOPOINT = "POINTTOPOINT";
+	public static final String MULTIPOINTTOMULTIPOINT = "MULTIPOINTTOMULTIPOINT";
+	
+}
